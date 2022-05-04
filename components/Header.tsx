f864b873568137e4afdc88f5af84205795ca7df1
Oriol Torrent Florensa <oriol.tf@me.com>
2022-05-04 10:09:53 +0200

    style: :lipstick: Improve Navigation
    Remove unused Home link, use next/link for improved navigation, remove mobile navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { useSession } from 'next-auth/client'
 import {sIn, sOut} from './helpers';
 
@@ -16,21 +17,19 @@ export default function Header() {
 	return (
 		<div className="row mb-4">
 			<div className="col-lg-12">
-				<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<nav className="navbar navbar-expand navbar-light bg-light">
 					<div className="container-fluid">
-						<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-							<span className="navbar-toggler-icon"></span>
-						</button>
-
-						<a className="navbar-brand" href="#">Home</a>
-
 						<div className="collapse navbar-collapse" id="navbarSupportedContent">
 							<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 								<li className="nav-item">
-									<a className="nav-link active" href="/">Home <span className="sr-only">(current)</span></a>
+									<Link href="/">
+										<a className="nav-link active">Home</a>
+									</Link>
 								</li>
 								<li className="nav-item">
-									<a className="nav-link" href="dashboard">Movies</a>
+									<Link href="/dashboard">
+										<a className="nav-link">Dashboard (protected)</a>
+									</Link>
 								</li>
 							</ul>
 							<div className="d-flex">
