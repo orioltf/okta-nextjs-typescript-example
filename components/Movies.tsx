95b3549d6ed28db55f04dec0b3c76ca313ef453f
Oriol Torrent Florensa <oriol.tf@me.com>
2022-05-05 15:43:08 +0200

    feat: :sparkles: Add claims to client session data

--- components/Movies.tsx
+++ components/Movies.tsx
@@ -1,8 +1,12 @@
+import {useSession} from 'next-auth/client';
+
 export default function Movies() {
+
+	const [...session] = useSession();
 
 	return (
 		<div className="row">
-			<div className="col-lg-4">
+			<div className="col-sm">
 				<div className="card">
 					<img className="card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/91gDYZG%2BzDL._AC_SL1500_.jpg" alt="Card image cap"></img>
 					<div className="card-body">
@@ -12,7 +16,7 @@ export default function Movies() {
 				</div>
 			</div>
 
-			<div className="col-lg-4">
+			<div className="col-sm">
 				<div className="card">
 					<img className="card-img-top" src="https://m.media-amazon.com/images/M/MV5BZjRlNDUxZjAtOGQ4OC00OTNlLTgxNmQtYTBmMDgwZmNmNjkxXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg" alt="Card image cap"></img>
 					<div className="card-body">
@@ -22,12 +26,20 @@ export default function Movies() {
 				</div>
 			</div>
 
-			<div className="col-lg-4">
+			<div className="col-sm">
 				<div className="card">
 					<img className="card-img-top" src="https://belgianfilmfreak.files.wordpress.com/2017/01/d0.jpg" alt="Card image cap"></img>
 					<div className="card-body">
 						<h5 className="card-title">Die Hard: With a Vengence</h5>
 						<p className="card-text">Think Fast, Look Alive, Die Hard</p>
+					</div>
+				</div>
+			</div>
+
+			<div>
+				<div className="card">
+					<div className="card-body">
+						<pre>{JSON.stringify(session, null, '\t')}</pre>
 					</div>
 				</div>
 			</div>
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,10 +7,39 @@ const options = {
 		Providers.Okta({
 			clientId: process.env.OKTA_CLIENTID,
 			clientSecret: process.env.OKTA_CLIENTSECRET,
-			domain: process.env.OKTA_DOMAIN
+			domain: process.env.OKTA_DOMAIN,
 		}),
 		// ...add more providers here
-	]
+	],
+	callbacks: {
+		async signin(...args) {
+			console.log('signin', args);
+			return true;
+		},
+		async session(...args) {
+			console.log('session', args);
+			return Promise.resolve({...args })
+		},
+		/**
+		 * This callback is called whenever a JSON Web Token is created (i.e. at sign in) or updated (i.e whenever a session is accessed in the client).
+		 * @param token
+		 * @param user - The claims in Okta
+		 * @param account - Large object with all info from the JWT
+		 * @param profile - Same as user, without `id`
+		 * @param {boolean} isNewUser
+		 * @param {[]} rest - should be empty
+		 */
+		async jwt(token, user, account, profile, isNewUser, ...rest) {
+			console.log('jwt', token, user, account, profile, isNewUser, rest.length, rest);
+			const newToken = {...token};
+			if (!newToken.claims) {
+				newToken.claims = user;
+				newToken.access_token = account.access_token;
+				newToken.accessToken = account.accessToken;
+			}
+			return newToken;
+		},
+	},
 }
 
 export default (req, res) => NextAuth(req, res, options)
